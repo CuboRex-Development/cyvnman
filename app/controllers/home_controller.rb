3d407640d3ba1a5c6532cf1f930d1285498b2e57
kyk-k <kishi-yuusuke@cuborex.com>
2024-07-26 20:42:29 +0900

    add: ダッシュボード

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
+    @types_count = Type.count
+    @models_count = Model.count
+    @blocks_count = Block.count
+    @parts_count = Part.count
+    @versions_count = Version.count
+
+    @recent_parts = Part.order(created_at: :desc).limit(5)
+    @recent_blocks = Block.order(created_at: :desc).limit(5)
+    @recent_models = Model.order(created_at: :desc).limit(5)
   end
 end
