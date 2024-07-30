49094338dc0479a5d6ca7a4d0dabf3a2b1c51825
kyk-k <kishi-yuusuke@cuborex.com>
2024-07-30 23:42:58 +0900

    fix: javascript の読み込み方法を変更

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,21 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//= require jquery3
-//= require popper
-//= require bootstrap
 
-import "./popper"
-import "./bootstrap"
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "bootstrap"
+import jquery from "jquery"
+import "select2"
+
+window.jQuery = jquery
+window.$ = jquery
+
+document.addEventListener("turbo:load", function() {
+    $('.select2').select2({
+        theme: 'bootstrap-5',
+        width: '100%'
+    });
+});
 
 document.addEventListener('turbolinks:load', () => {
     const typeSelect = document.querySelector('#block_type_id');
