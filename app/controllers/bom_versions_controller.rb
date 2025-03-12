d8ef30a57162a7cc925905a914c44d258cf56108
kyk-k <kishi-yuusuke@cuborex.com>
2025-03-12 21:10:33 +0900

    add: 基本機能を実装

---
+++ app/controllers/bom_versions_controller.rb
@@ -0,0 +1,13 @@
+class BomVersionsController < ApplicationController
+  before_action :authenticate_user!, except: %i[index show]
+
+  # GET /bom_versions
+  def index
+    @bom_versions = BomVersion.order(fixed_at: :desc)
+  end
+
+  # GET /bom_versions/:id
+  def show
+    @bom_version = BomVersion.find(params[:id])
+  end
+end
